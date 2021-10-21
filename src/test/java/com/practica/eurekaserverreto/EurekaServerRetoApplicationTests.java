cd1ebb37ca9c3c21d67b4585bed16b94092abfcc
Peluconus <peluconus@hotmail.com>
2021-10-21 14:45:42 +0200

    Servidor Eureka reto

---
+++ src/test/java/com/practica/eurekaserverreto/EurekaServerRetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.eurekaserverreto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerRetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
